e70f5b808ecbfbe5e1a1b7defda141375cb7ae4d
Alberto Esteban <jaestebana@minsait.com>
2021-11-09 15:15:04 +0100

    vuex locale

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,23 @@
 </template>
 
 <script>
+  
+  import { mapGetters } from 'vuex'
 
   export default {
     name: 'App',
     components: {},
+    methods: {
+      setLocale(locale) {
+      this.$i18n.locale = locale
+      this.$store.dispatch('setLocale', locale)
+      }
+    },
     mounted: function () {
-      this.$i18n.locale = 'ES'
+      this.setLocale('EN')
+    },
+    computed: {
+      ...mapGetters(['getLocale']),
     }
   }
 
--- src/store.js
+++ src/store.js
@@ -5,13 +5,20 @@ const store = createStore({
   },
   state() {
     return {
+      locale: 'ES'
     };
   },
   mutations: {
+    setLocale(state, locale) { state.locale = locale }
   },
   actions: {
+    setLocale(context, locale) {
+      sessionStorage.setItem('locale', locale)
+      context.commit('setLocale', locale)
+    }
   },
   getters: {
+    getLocale(state) { return state.locale }
   }
 });
 
