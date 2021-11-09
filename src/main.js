b26dcc4a356826b9649101bb24612990bd4a5e65
Alberto Esteban <jaestebana@minsait.com>
2021-11-09 15:10:03 +0100

    vuex config

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 
 import router from './router.js';
 import i18n from './i18n.js';
+import store from './store.js';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(i18n);
+app.use(store);
 
 app.mount('#app');
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  modules: {
+  },
+  state() {
+    return {
+    };
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  getters: {
+  }
+});
+
+export default store;
